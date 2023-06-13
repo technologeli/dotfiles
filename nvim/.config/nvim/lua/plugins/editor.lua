81a0232d366a54769de3cec10081c11bec41cd8e
Elijah Lieu <50640682+technologeli@users.noreply.github.com>
2023-06-13 18:40:03 -0400

    whichkey and todo]

--- nvim/.config/nvim/lua/plugins/editor.lua
+++ nvim/.config/nvim/lua/plugins/editor.lua
@@ -2,19 +2,30 @@ return {
     "tpope/vim-sleuth",
     {
         "numToStr/Comment.nvim",
-        lazy = true,
+        event = "VeryLazy",
         config = true
     },
     {
         "folke/trouble.nvim",
         dependencies = { "nvim-tree/nvim-web-devicons" },
         keys = {
-            { "<leader>t", vim.cmd.TroubleToggle, desc = "[T]rouble" }
+            { "<leader>tp", "<cmd>Trouble workspace_diagnostics<cr>", desc = "[T]rouble [P]roject" },
+            { "<leader>tt", "<cmd>Trouble todo<cr>",                  desc = "[T]rouble [T]odo" },
+            { "<leader>tw", "<cmd>TroubleClose<cr>",                  desc = "Trouble Close" }
         }
     },
     {
         "folke/todo-comments.nvim",
         dependencies = { "nvim-lua/plenary.nvim" },
-        config = true
+        config = true,
     },
+    {
+        "folke/which-key.nvim",
+        event = "VeryLazy",
+        init = function()
+            vim.o.timeout = true
+            vim.o.timeoutlen = 300
+        end,
+        config = true
+    }
 }
