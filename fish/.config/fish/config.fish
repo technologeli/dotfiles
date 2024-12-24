e0966bf37848cbc668d45fbed34dd3ac3d212c4a
Elijah Lieu <50640682+technologeli@users.noreply.github.com>
2024-12-24 11:51:45 -0500

    Add bat, neovim manpager

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -6,6 +6,7 @@ fish_add_path ~/.local/bin
 
 if status is-interactive
 	set -Ux EDITOR nvim
+	set -Ux MANPAGER "nvim +Man!"
 	fish_vi_key_bindings
 	zoxide init fish --cmd cd | source
 	fzf_key_bindings
@@ -13,4 +14,5 @@ if status is-interactive
 
 	alias ls "eza --color=auto"
 	alias grep "grep --color=auto"
+	alias bat "batcat"
 end
