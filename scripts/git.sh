32a89932606d29cd35a36fc423531078de6925fc
Elijah Lieu <50640682+technologeli@users.noreply.github.com>
2025-02-07 10:24:05 -0500

    Update git script

--- scripts/git.sh
+++ scripts/git.sh
@@ -1,24 +1,55 @@
 #!/usr/bin/env bash
 
+# colors!
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+RESET='\033[0m' # reset
 
-pushd ~/dotfiles
-git status
-popd
+# defaults
+pull=false
+push=false
 
-echo
-echo
-echo
-echo
+while [ $# -gt 0 ]; do
+	case "$1" in
+		--pull)
+			pull=true
+			shift
+			;;
+		--push)
+			push=true
+			shift
+			;;
+		--all)
+			pull=true
+			push=true
+			shift
+			;;
+		*)
+			echo "Unknown option: $1"
+			exit 1
+			;;
+	esac
+done
 
-pushd ~/topaz
-git status
-popd
+run() {
+	echo -e "${GREEN}========== ${YELLOW}$1${GREEN} ==========${RESET}"
+	pushd $1 > /dev/null 2>&1
+	if [ "$pull" = true ]; then
+		git pull
+	fi
+	git status
+	if [ "$push" = true ]; then
+		git add .
+		git commit
+	fi
+	popd > /dev/null 2>&1
+	echo
+}
 
-echo
-echo
-echo
-echo
-
-pushd ~/.password-store
-git status
-popd
+run ~/dotfiles
+run ~/topaz
+run ~/.password-store
