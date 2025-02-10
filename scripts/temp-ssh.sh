29ac530642e154029f80971f1f14ca7b419e2a86
Elijah Lieu <50640682+technologeli@users.noreply.github.com>
2025-02-10 12:26:14 -0500

    Fix temp-ssh.sh

--- scripts/temp-ssh.sh
+++ scripts/temp-ssh.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-ssh-add -t 1m "$HOME/.ssh/id_ed25519" 2>/dev/null
+ssh-add -l > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+	echo "SSH key expired, adding for one minute..."
+	ssh-add -t 1m "$HOME/.ssh/id_ed25519" 2>/dev/null
+fi
