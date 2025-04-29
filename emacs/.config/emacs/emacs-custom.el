9c5efc52b13f088f861860e3a3432c6556fac34f
Elijah Lieu <50640682+technologeli@users.noreply.github.com>
2025-04-29 16:02:10 -0400

    org configuration

--- emacs/.config/emacs/emacs-custom.el
+++ emacs/.config/emacs/emacs-custom.el
@@ -4,12 +4,13 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("4594d6b9753691142f02e67b8eb0fda7d12f6cc9f1299a49b819312d6addad1d"
+   '("48042425e84cd92184837e01d0b4fe9f912d875c43021c3bcb7eeb51f1be5710"
+     "4594d6b9753691142f02e67b8eb0fda7d12f6cc9f1299a49b819312d6addad1d"
      default))
  '(package-selected-packages
    '(consult doom-modeline doom-themes evil general magit marginalia
-	     nerd-icons-dired orderless org-appear spacious-padding
-	     undo-tree vertico vterm)))
+	     nerd-icons-dired orderless org-appear org-bullets
+	     spacious-padding undo-tree vertico vterm)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -1,163 +1,185 @@
-(setq custom-file "~/.config/emacs/emacs-custom.el")
+  (setq custom-file "~/.config/emacs/emacs-custom.el")
 
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")
-			 ("melpa-stable" . "https://stable.melpa.org/packages/")
-			 ("org" . "https://orgmode.org/elpa/")
-			 ("elpa" . "https://elpa.gnu.org/packages/")))
-(package-initialize)
-(unless package-archive-contents
-  (package-refresh-contents))
+  (setq package-archives '(("melpa" . "https://melpa.org/packages/")
+  			 ("melpa-stable" . "https://stable.melpa.org/packages/")
+  			 ("org" . "https://orgmode.org/elpa/")
+  			 ("elpa" . "https://elpa.gnu.org/packages/")))
+  (package-initialize)
+  (unless package-archive-contents
+    (package-refresh-contents))
 
-;; Initialize use-package
-;; -p means "predicate"
-(unless (package-installed-p 'use-package)
-  (package-install 'use-package))
-(require 'use-package)
-;; Install all packages specified by use-package
-(setq use-package-always-ensure t)
+  ;; Initialize use-package
+  ;; -p means "predicate"
+  (unless (package-installed-p 'use-package)
+    (package-install 'use-package))
+  (require 'use-package)
+  ;; Install all packages specified by use-package
+  (setq use-package-always-ensure t)
 
-(setq inhibit-startup-message t)
-(setq ring-bell-function 'ignore)
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-(setq-default display-line-numbers 'relative)
+  (setq inhibit-startup-message t)
+  (setq ring-bell-function 'ignore)
+  (menu-bar-mode -1)
+  (tool-bar-mode -1)
+  (scroll-bar-mode -1)
+  (setq-default display-line-numbers 'relative)
 
-(defun vitix/init-fonts ()
-  (setq vitix/fixed-font-height 150)
-  (setq vitix/variable-font-height 140)
-  (set-face-attribute 'default nil :font "Iosevka Fixed SS14" :height vitix/fixed-font-height)
-  (set-face-attribute 'fixed-pitch nil :font "Iosevka Fixed SS14" :height vitix/fixed-font-height)
-  (set-face-attribute 'line-number nil :font "Iosevka Fixed SS14" :height vitix/fixed-font-height)
-  (set-face-attribute 'line-number-current-line nil :font "Iosevka Fixed SS14" :height vitix/fixed-font-height)
-  (set-face-attribute 'variable-pitch nil :font "Iosevka Aile" :height vitix/variable-font-height)
-  )
-(vitix/init-fonts)
+  (defun vitix/init-fonts ()
+    (setq vitix/fixed-font-height 150)
+    (setq vitix/variable-font-height 140)
+    (setq vitix/fixed-font "JetBrains Mono")
+    (setq vitix/variable-font "Inter")
+    (set-face-attribute 'default nil :font vitix/fixed-font :height vitix/fixed-font-height)
+    (set-face-attribute 'fixed-pitch nil :font vitix/fixed-font :height vitix/fixed-font-height)
+    (set-face-attribute 'line-number nil :font vitix/fixed-font  :height vitix/fixed-font-height)
+    (set-face-attribute 'line-number-current-line nil :font vitix/fixed-font :height vitix/fixed-font-height)
+    (set-face-attribute 'variable-pitch nil :font vitix/variable-font :height vitix/variable-font-height)
+    )
+  (vitix/init-fonts)
 
-;; run M-x nerd-icons-install-fonts after installing
-(use-package nerd-icons)
-(use-package nerd-icons-dired
-  :hook
-  (dired-mode . nerd-icons-dired-mode))
-(use-package doom-modeline
-  :config
-  (setq doom-modeline-modal-modern-icon nil)
-  :init
-  (doom-modeline-mode 1))
+  ;; run M-x nerd-icons-install-fonts after installing
+  (use-package nerd-icons)
+  (use-package nerd-icons-dired
+    :hook
+    (dired-mode . nerd-icons-dired-mode))
+  (use-package doom-modeline
+    :config
+    (setq doom-modeline-modal-modern-icon nil)
+    :init
+    (doom-modeline-mode 1))
 
-(use-package doom-themes
-  :config
-  (setq doom-themes-enable-bold t)
-  (setq doom-themes-enable-italic t)
-  (load-theme 'doom-gruvbox t)
-  (doom-themes-org-config))
+  (use-package doom-themes
+    :config
+    (setq doom-themes-enable-bold t)
+    (setq doom-themes-enable-italic t)
+    (load-theme 'doom-gruvbox t)
+    (doom-themes-org-config))
 
-(use-package spacious-padding
-  :init
-  (spacious-padding-mode 1))
+  (use-package spacious-padding
+    :init
+    (spacious-padding-mode 1))
 
-(setq make-backup-files nil)
-(use-package undo-tree
-  :config
-  (setq undo-tree-history-directory-alist '(("." . "~/.cache/emacs/undo/")))
-  :init
-  (global-undo-tree-mode))
+  (setq make-backup-files nil)
+  (use-package undo-tree
+    :config
+    (setq undo-tree-history-directory-alist '(("." . "~/.cache/emacs/undo/")))
+    :init
+    (global-undo-tree-mode))
 
-(use-package evil
-  :init
-  (setq evil-undo-system 'undo-tree)
-  (setq evil-want-C-u-scroll t)
-  (setq evil-want-Y-yank-to-eol t)
-  :config
-  (evil-mode 1)
-  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
-  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
-  )
-
-;; vterm requires libtool-bin
-(use-package vterm
-  :config
-  (setq vterm-shell "/usr/bin/fish")
-  )
-
-(use-package vertico
-  :init
-  (vertico-mode 1)
-  (savehist-mode 1)
-  (add-hook 'rfn-eshadow-update-overlay-hook #'vertico-directory-tidy))
-
-(use-package consult)
-
-(use-package marginalia
-  :init
-  (marginalia-mode 1))
-
-(use-package orderless
-  :custom
-  (completion-styles '(orderless basic))
-  (completion-category-overrides '((file (styles basic partial-completion)))))
-
-;; Keybinds
-(which-key-mode t)
-(use-package general
-  :config
-  (general-evil-setup t)
-  (general-create-definer vitix/keymap
-    :keymaps '(normal insert visual emacs)
-    :prefix "SPC"
-    :global-prefix "C-SPC")
-  (vitix/keymap
-    "SPC" '(consult-buffer :which-key "Consult Buffer")
-    "C-SPC" '(consult-buffer :which-key "Consult Buffer")
-    "f" '(consult-find :which-key "Consult [F]ind")
-    "t" '(vterm :which-key "[T]erminal")
-
-    "h" '(:ignore t :which-key "[H]arpoon")
-    "hs" '(bookmark-save :which-key "Harpoon [S]ave")
-    "hl" '(bookmark-load :which-key "Harpoon [L]oad")
-    "hf" '(consult-bookmark :which-key "Harpoon [F]ind")
-    "hd" '(bookmark-delete :which-key "Harpoon [D]elete")
+  (use-package evil
+    :init
+    (setq evil-undo-system 'undo-tree)
+    (setq evil-want-C-u-scroll t)
+    (setq evil-want-Y-yank-to-eol t)
+    :config
+    (evil-mode 1)
+    (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+    (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
     )
 
-  (general-define-key
-   :states 'normal
-   "-" #'dired-jump
-   "zf" #'evil-toggle-fold)
+  ;; vterm requires libtool-bin
+  (use-package vterm
+    :config
+    (setq vterm-shell "/usr/bin/fish")
+    )
 
-  (general-define-key
-   :keymaps 'dired-mode-map
-   "-" #'dired-up-directory)
+  (use-package vertico
+    :init
+    (vertico-mode 1)
+    (savehist-mode 1)
+    (add-hook 'rfn-eshadow-update-overlay-hook #'vertico-directory-tidy))
 
-  (general-define-key
-   :keymaps 'vterm-mode-map
-   "C-S-v" #'vterm-yank)
-  )
+  (use-package consult)
 
-(add-hook 'dired-mode-hook #'dired-hide-details-mode)
+  (use-package marginalia
+    :init
+    (marginalia-mode 1))
 
-(use-package magit)
+  (use-package orderless
+    :custom
+    (completion-styles '(orderless basic))
+    (completion-category-overrides '((file (styles basic partial-completion)))))
 
-(defun vitix/org-mode-setup ()
-  (org-indent-mode)
-  (variable-pitch-mode)
-  (vitix/init-fonts)
-  (set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
-  (set-face-attribute 'org-code nil   :inherit '(shadow fixed-pitch))
-  (set-face-attribute 'org-indent nil :inherit '(org-hide fixed-pitch))
-  (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
-  (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
-  (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
-  (set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch)
-  )
-(use-package org
-  :hook (org-mode . vitix/org-mode-setup)
-  :config
-  (setq org-hide-emphasis-markers t)
-  )
+  ;; Keybinds
+  (which-key-mode t)
+  (use-package general
+    :config
+    (general-evil-setup t)
+    (general-create-definer vitix/keymap
+      :keymaps '(normal insert visual emacs)
+      :prefix "SPC"
+      :global-prefix "C-SPC")
+    (vitix/keymap
+      "SPC" '(consult-buffer :which-key "Consult Buffer")
+      "C-SPC" '(consult-buffer :which-key "Consult Buffer")
+      "f" '(consult-find :which-key "Consult [F]ind")
+      "t" '(vterm :which-key "[T]erminal")
 
-(use-package org-appear
-  :init
-  (add-hook 'org-mode-hook 'org-appear-mode)
-  )
+      "h" '(:ignore t :which-key "[H]arpoon")
+      "hs" '(bookmark-save :which-key "Harpoon [S]ave")
+      "hl" '(bookmark-load :which-key "Harpoon [L]oad")
+      "hf" '(consult-bookmark :which-key "Harpoon [F]ind")
+      "hd" '(bookmark-delete :which-key "Harpoon [D]elete")
+      )
 
-(load custom-file)
+    (general-define-key
+     :states 'normal
+     "-" #'dired-jump
+     "zf" #'evil-toggle-fold)
+
+    (general-define-key
+     :keymaps 'dired-mode-map
+     "-" #'dired-up-directory)
+
+    (general-define-key
+     :keymaps 'vterm-mode-map
+     "C-S-v" #'vterm-yank)
+    )
+
+  (add-hook 'dired-mode-hook #'dired-hide-details-mode)
+
+  (use-package magit)
+
+  (defun vitix/org-mode-setup ()
+    (org-indent-mode)
+    (variable-pitch-mode)
+    (vitix/init-fonts)
+    (dolist (face '((org-level-1 . 2.0)
+                    (org-level-2 . 1.5)
+                    (org-level-3 . 1.25)
+                    (org-level-4 . 1.0)
+                    (org-level-5 . 1.0)
+                    (org-level-6 . 1.0)
+                    (org-level-7 . 1.0)
+                    (org-level-8 . 1.0)))
+    (set-face-attribute (car face) nil :font vitix/variable-font :height (cdr face)))
+    (set-face-attribute 'org-block nil :foreground nil :background "unspecified-bg" :inherit 'fixed-pitch)
+    (set-face-attribute 'org-code nil :inherit '(shadow fixed-pitch))
+    (set-face-attribute 'org-indent nil :inherit '(org-hide fixed-pitch))
+    (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+    (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+    (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+    (set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch)
+    )
+  (use-package org
+    :hook (org-mode . vitix/org-mode-setup)
+    :config
+    (setq org-hide-emphasis-markers t)
+    (setq org-src-preserve-indentation t)
+    )
+
+  (use-package org-appear
+    :init
+    (add-hook 'org-mode-hook 'org-appear-mode)
+    )
+
+  (use-package org-bullets
+    :after org
+    :hook (org-mode . org-bullets-mode)
+    :custom
+    (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
+
+(setq org-structure-template-alist '(("s" . "src")
+				     ("e" . "src emacs-lisp")
+				     ("p" . "src python")))
+
+  (load custom-file)
