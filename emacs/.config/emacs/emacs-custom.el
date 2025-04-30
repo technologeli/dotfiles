cc45696d32de89bb5026f7eca8e6201f28043c60
Elijah Lieu <50640682+technologeli@users.noreply.github.com>
2025-04-30 00:31:12 -0400

    more emacs

--- emacs/.config/emacs/emacs-custom.el
+++ emacs/.config/emacs/emacs-custom.el
@@ -4,13 +4,15 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("48042425e84cd92184837e01d0b4fe9f912d875c43021c3bcb7eeb51f1be5710"
+   '("77f281064ea1c8b14938866e21c4e51e4168e05db98863bd7430f1352cab294a"
+     "48042425e84cd92184837e01d0b4fe9f912d875c43021c3bcb7eeb51f1be5710"
      "4594d6b9753691142f02e67b8eb0fda7d12f6cc9f1299a49b819312d6addad1d"
      default))
  '(package-selected-packages
-   '(consult doom-modeline doom-themes evil general magit marginalia
-	     nerd-icons-dired orderless org-appear org-bullets
-	     spacious-padding undo-tree vertico vterm)))
+   '(consult consult-denote denote denote-journal doom-modeline
+	     doom-themes ef-themes evil general magit marginalia
+	     modus-themes nerd-icons-dired orderless org-appear
+	     org-bullets spacious-padding undo-tree vertico vterm)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -21,17 +21,16 @@
   (menu-bar-mode -1)
   (tool-bar-mode -1)
   (scroll-bar-mode -1)
+  (blink-cursor-mode -1)
   (setq-default display-line-numbers 'relative)
 
   (defun vitix/init-fonts ()
-    (setq vitix/fixed-font-height 150)
+    (setq vitix/fixed-font-height 140)
     (setq vitix/variable-font-height 140)
     (setq vitix/fixed-font "JetBrains Mono")
     (setq vitix/variable-font "Inter")
     (set-face-attribute 'default nil :font vitix/fixed-font :height vitix/fixed-font-height)
     (set-face-attribute 'fixed-pitch nil :font vitix/fixed-font :height vitix/fixed-font-height)
-    (set-face-attribute 'line-number nil :font vitix/fixed-font  :height vitix/fixed-font-height)
-    (set-face-attribute 'line-number-current-line nil :font vitix/fixed-font :height vitix/fixed-font-height)
     (set-face-attribute 'variable-pitch nil :font vitix/variable-font :height vitix/variable-font-height)
     )
   (vitix/init-fonts)
@@ -41,18 +40,40 @@
   (use-package nerd-icons-dired
     :hook
     (dired-mode . nerd-icons-dired-mode))
-  (use-package doom-modeline
-    :config
-    (setq doom-modeline-modal-modern-icon nil)
-    :init
-    (doom-modeline-mode 1))
+   (use-package doom-modeline
+     :config
+     (setq doom-modeline-modal-modern-icon nil)
+     (setq doom-modeline-height 25)
+     (setq doom-modeline-position-line-format '(""))
+     (setq doom-modeline-percent-position '(-3 ""))
+     :init
+     (doom-modeline-mode 1))
 
-  (use-package doom-themes
+  ;; (use-package doom-themes
+  ;;   :config
+  ;;   (setq doom-themes-enable-bold t)
+  ;;   (setq doom-themes-enable-italic t)
+  ;;   (load-theme 'doom-gruvbox t)
+  ;;   (doom-themes-org-config))
+
+
+  (use-package ef-themes
+    :init
+    (setq ef-themes-mixed-fonts t)
+    (setq ef-themes-bold-constructs t)
+    (setq ef-themes-italic-constructs t)
+    (setq ef-themes-variable-pitch-ui nil)
+    (setq ef-themes-prompts '(bold))
+    (setq ef-themes-completions '((matches . (bold))
+				     (selection . ())))
+    (setq ef-themes-to-toggle '(ef-dream ef-kassio))
+    (setq ef-themes-headings '((1 . (1.2))
+			       (2 . (1.15))
+			       (3 . (1.1))
+			       (t . (1.05))))
     :config
-    (setq doom-themes-enable-bold t)
-    (setq doom-themes-enable-italic t)
-    (load-theme 'doom-gruvbox t)
-    (doom-themes-org-config))
+    (ef-themes-load-theme 'ef-dream)
+    )
 
   (use-package spacious-padding
     :init
@@ -113,6 +134,7 @@
       "C-SPC" '(consult-buffer :which-key "Consult Buffer")
       "f" '(consult-find :which-key "Consult [F]ind")
       "t" '(vterm :which-key "[T]erminal")
+      "e" '(ef-themes-toggle :which-key "[E]f Themes Toggle")
 
       "h" '(:ignore t :which-key "[H]arpoon")
       "hs" '(bookmark-save :which-key "Harpoon [S]ave")
@@ -140,25 +162,8 @@
   (use-package magit)
 
   (defun vitix/org-mode-setup ()
-    (org-indent-mode)
     (variable-pitch-mode)
-    (vitix/init-fonts)
-    (dolist (face '((org-level-1 . 2.0)
-                    (org-level-2 . 1.5)
-                    (org-level-3 . 1.25)
-                    (org-level-4 . 1.0)
-                    (org-level-5 . 1.0)
-                    (org-level-6 . 1.0)
-                    (org-level-7 . 1.0)
-                    (org-level-8 . 1.0)))
-    (set-face-attribute (car face) nil :font vitix/variable-font :height (cdr face)))
-    (set-face-attribute 'org-block nil :foreground nil :background "unspecified-bg" :inherit 'fixed-pitch)
-    (set-face-attribute 'org-code nil :inherit '(shadow fixed-pitch))
-    (set-face-attribute 'org-indent nil :inherit '(org-hide fixed-pitch))
-    (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
-    (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
-    (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
-    (set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch)
+    (org-indent-mode)
     )
   (use-package org
     :hook (org-mode . vitix/org-mode-setup)
@@ -181,5 +186,37 @@
 (setq org-structure-template-alist '(("s" . "src")
 				     ("e" . "src emacs-lisp")
 				     ("p" . "src python")))
+
+  (use-package denote
+    :hook (dired-mode . denote-dired-mode)
+    :bind
+    (("C-c n n" . denote)
+     ("C-c n r" . denote-rename-file)
+     ("C-c n l" . denote-link)
+     ("C-c n d" . denote-dired)
+     ("C-c n g" . denote-grep))
+    :config
+    (setq denote-directory (expand-file-name "~/tome"))
+    (denote-rename-buffer-mode 1)
+    (setq denote-known-keywords '()))
+
+  (use-package consult-denote
+    :bind
+    (("C-c n f" . consult-denote-find)
+     ("C-c n g" . consult-denote-grep))
+    :config
+    (consult-denote-mode 1))
+
+  (use-package denote-journal
+    :hook (calendar-mode . denote-journal-calendar-mode)
+    :bind
+    (("C-c n t" . denote-journal-new-or-existing-entry)
+     ("C-c n s" . denote-journal-link-or-create-entry))
+    :config
+    ;; save journal entries in denote-directory
+    (setq denote-journal-directory nil)
+    (setq denote-journal-keyword "journal")
+    (setq denote-journal-title-format 'day-date-month-year)
+    )
 
   (load custom-file)
