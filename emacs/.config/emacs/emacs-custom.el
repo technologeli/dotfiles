d32e455a5375f78a2ca9d082c3e17d31ce9d7f64
Elijah Lieu <50640682+technologeli@users.noreply.github.com>
2025-05-08 19:50:21 -0400

    remote emacs

--- emacs/.config/emacs/emacs-custom.el
+++ emacs/.config/emacs/emacs-custom.el
@@ -3,7 +3,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages nil))
+ '(package-selected-packages
+   '(cape company consult-denote corfu denote-journal denote-org
+	  doom-modeline ef-themes evil flycheck general lsp-pyright
+	  lsp-ui magit marginalia nerd-icons-dired orderless
+	  org-appear org-bullets pyvenv undo-tree vertico vterm)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -51,7 +51,8 @@
     (setq ef-themes-completions '((matches . (bold))
 				     (selection . ())))
     (setq ef-themes-to-toggle '(ef-dream ef-kassio))
-    (setq ef-themes-headings '((1 . (1.2))
+    (setq ef-themes-headings '((0 . (1.75))
+			       (1 . (1.2))
 			       (2 . (1.15))
 			       (3 . (1.1))
 			       (t . (1.05))))
@@ -116,6 +117,7 @@
   (defun vitix/org-mode-setup ()
     (variable-pitch-mode)
     (vitix/init-fonts)
+    (visual-line-mode)
     (org-indent-mode)
     )
   (use-package org
@@ -133,160 +135,171 @@
     (add-hook 'org-mode-hook 'org-appear-mode)
     )
 
-  (use-package org-bullets
-    :after org
-    :hook (org-mode . org-bullets-mode)
-    :custom
-    (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
+(use-package org-bullets
+  :after org
+  :hook (org-mode . org-bullets-mode)
+  :custom
+  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
 
 (setq org-structure-template-alist '(("s" . "src")
-				     ("e" . "src emacs-lisp")
-				     ("p" . "src python")))
+                                     ("e" . "src emacs-lisp")
+                                     ("p" . "src python")))
 
-  (use-package org-capture
-    :ensure nil ; org-capture comes with emacs, just use this to configure it
-    :config
-    (setq org-capture-templates
-	  '(("l" "Log" entry
-	    (file+headline denote-journal-path-to-new-or-existing-entry "Log")
-	    "* %<%I:%M %p> - %?"
-	    )
-	    ("t" "Task" entry
-	    (file+headline denote-journal-path-to-new-or-existing-entry "Task")
-            "* TODO %?"
-	    )))
-    :bind
-    ("C-c c" . org-capture))
-
-  (setq org-todo-keywords '((sequence
-			     "TODO(t)"
-			     "WAIT(w@/!)"
-			     "|"
-			     "DONE(d/!)"
-			     "STOP(s@/!)")))
-  (use-package org-agenda
-    :ensure nil
-    :config
-    (setq org-agenda-files (list org-directory))
-    :bind
-    ("C-c a" . org-agenda))
-
-  (use-package denote
-    :hook (dired-mode . denote-dired-mode)
-    :bind
-    (("C-c n n" . denote)
-     ("C-c n r" . denote-rename-file)
-     ("C-c n l" . denote-link)
-     ("C-c n d" . denote-dired)
-     ("C-c n g" . denote-grep))
-    :config
-    (setq denote-directory (expand-file-name "~/tome"))
-    (denote-rename-buffer-mode 1)
-    (setq denote-known-keywords '()))
-
-  (use-package consult-denote
-    :bind
-    (("C-c n f" . consult-denote-find)
-     ("C-c n g" . consult-denote-grep))
-    :config
-    (consult-denote-mode 1))
-
-  (use-package denote-journal
-    :hook (calendar-mode . denote-journal-calendar-mode)
-    :bind
-    (("C-c n t" . denote-journal-new-or-existing-entry)
-     ("C-c n s" . denote-journal-link-or-create-entry))
-    :config
-    ;; save journal entries in denote-directory
-    (setq denote-journal-directory nil)
-    (setq denote-journal-keyword "journal")
-    (setq denote-journal-title-format 'day-date-month-year)
-    )
-
-  (add-to-list 'exec-path "/home/eli/.volta/bin")
-  (add-to-list 'exec-path "/home/eli/.local/bin")
-
-(use-package lsp-mode
-  :init
-  (setq lsp-keymap-prefix "C-l")
-  (setq lsp-headerline-breadcrumb-enable nil)
-  (setq gc-cons-threshold 100000000)
-  :hook
-  (python-mode . lsp)
-  (c-mode . lsp)
-  (c++-mode . lsp)
-  (lsp-mode . lsp-enable-which-key-integration)
-  :commands lsp)
-
-(use-package lsp-ui
-  :init
-  (setq lsp-ui-doc-position 'at-point)
-  :commands lsp-ui-mode)
-
-(use-package company
+(use-package org-capture
+  :ensure nil ; org-capture comes with emacs, just use this to configure it
   :config
-  (setq company-minimum-prefix-length 1)
-  (setq company-idle-delay 0.0)
+  (setq org-capture-templates
+        '(("l" "Log" entry
+           (file+headline denote-journal-path-to-new-or-existing-entry "Log")
+           "* %<%I:%M %p> - %?"
+           )
+          ("t" "Task" entry
+           (file+headline denote-journal-path-to-new-or-existing-entry "Task")
+           "* TODO %?"
+           )
+          ("i" "TTRPG Idea" entry
+           (file+headline "20250507T140321--ttrpg-ideas__ttrpg.org" "Ideas")
+           "* %?")))
+  :bind
+  ("C-c c" . org-capture))
+
+(setq org-todo-keywords '((sequence
+                           "TODO(t)"
+                           "WAIT(w@/!)"
+                           "|"
+                           "DONE(d/!)"
+                           "STOP(s@/!)")))
+(setq org-todo-keyword-faces
+      `(("TODO" . ,(ef-themes-get-color-value 'green))
+	("WAIT" . ,(ef-themes-get-color-value 'yellow-warmer))
+	("DONE" . ,(ef-themes-get-color-value 'bg-dim))
+	("STOP" . ,(ef-themes-get-color-value 'fg-dim))))
+(set-face-attribute 'org-headline-done nil
+		    :foreground (ef-themes-get-color-value 'bg-dim))
+
+(use-package org-agenda
+  :ensure nil
+  :config
+  (setq org-agenda-files (list org-directory))
+  :bind
+  ("C-c a" . org-agenda))
+
+(use-package denote
+  :hook (dired-mode . denote-dired-mode)
+  :bind
+  (("C-c n n" . denote)
+   ("C-c n r" . denote-rename-file)
+   ("C-c n l" . denote-link)
+   ("C-c n d" . denote-dired)
+   ("C-c n g" . denote-grep))
+  :config
+  (setq denote-directory (expand-file-name "~/tome"))
+  (denote-rename-buffer-mode 1)
+  (setq denote-known-keywords '()))
+
+(use-package consult-denote
+  :bind
+  (("C-c n f" . consult-denote-find)
+   ("C-c n g" . consult-denote-grep))
+  :config
+  (consult-denote-mode 1))
+
+(use-package denote-journal
+  :hook (calendar-mode . denote-journal-calendar-mode)
+  :bind
+  (("C-c n t" . denote-journal-new-or-existing-entry)
+   ("C-c n s" . denote-journal-link-or-create-entry))
+  :config
+  ;; save journal entries in denote-directory
+  (setq denote-journal-directory nil)
+  (setq denote-journal-keyword "journal")
+  (setq denote-journal-title-format 'day-date-month-year)
   )
 
-(use-package flycheck
+(use-package denote-org)
+
+(add-to-list 'exec-path "/home/eli/.volta/bin")
+(add-to-list 'exec-path "/home/eli/.local/bin")
+
+(add-to-list 'tramp-remote-path 'tramp-own-remote-path)
+
+(use-package eglot
+  :ensure nil
   :config
-  (add-hook 'after-init-hook #'global-flycheck-mode))
+  (global-eldoc-mode t))
 
-  (use-package pyvenv
-    :config
-    (pyvenv-mode 1))
+(use-package corfu
+  :bind (:map corfu-map
+              ("M-SPC" . corfu-insert-separator)
+              ("M-y" . corfu-insert)
+              ("RET" . nil))
 
-  (use-package lsp-pyright
-    :custom (lsp-pyright-langserver-command "pyright")
-    :hook (python-mode . (lambda ()
-                            (require 'lsp-pyright)
-                            (lsp))))
+  :init
+  (setq corfu-auto t)
+  (global-corfu-mode t)
+  (corfu-history-mode t))
 
-  ;; Keybinds
-  (which-key-mode t)
-  (use-package general
-    :config
-    (general-evil-setup t)
-    (general-create-definer vitix/keymap
-      :keymaps '(normal insert visual emacs)
-      :prefix "SPC"
-      :global-prefix "C-SPC")
-    (vitix/keymap
-      "SPC" '(consult-buffer :which-key "Consult Buffer")
-      "C-SPC" '(consult-buffer :which-key "Consult Buffer")
-      "f" '(consult-find :which-key "Consult [F]ind")
-      "t" '(vterm :which-key "[T]erminal")
-      "e" '(ef-themes-toggle :which-key "[E]f Themes Toggle")
+(use-package cape
+  :bind ("C-c p" . cape-prefix-map)
+  :init
+  (advice-add 'eglot-completion-at-point :around #'cape-wrap-buster)
+  (add-hook 'completion-at-point-functions #'cape-file)
+  (add-hook 'completion-at-point-functions #'cape-elisp-block)
+  )
 
-      "h" '(:ignore t :which-key "[H]arpoon")
-      "hs" '(bookmark-save :which-key "Harpoon [S]ave")
-      "hl" '(bookmark-load :which-key "Harpoon [L]oad")
-      "hf" '(consult-bookmark :which-key "Harpoon [F]ind")
-      "hd" '(bookmark-delete :which-key "Harpoon [D]elete")
+(which-key-mode t)
+(use-package general
+  :config
+  (general-evil-setup t)
+  (general-create-definer vitix/keymap
+    :keymaps '(normal insert visual emacs)
+    :prefix "SPC"
+    :global-prefix "C-SPC")
+  (vitix/keymap
+    "SPC" '(consult-buffer :which-key "Consult Buffer")
+    "C-SPC" '(consult-buffer :which-key "Consult Buffer")
+    "f" '(consult-find :which-key "Consult [F]ind")
+    "t" '(vterm :which-key "[T]erminal")
+    "e" '(ef-themes-toggle :which-key "[E]f Themes Toggle")
 
-      "pa" '(pyvenv-activate :which-key "[P]yvenv [A]ctivate")
-      "pd" '(pyvenv-deactivate :which-key "[P]yvenv [D]eactivate")
+    "h" '(:ignore t :which-key "[H]arpoon")
+    "hs" '(bookmark-save :which-key "Harpoon [S]ave")
+    "hl" '(bookmark-load :which-key "Harpoon [L]oad")
+    "hf" '(consult-bookmark :which-key "Harpoon [F]ind")
+    "hd" '(bookmark-delete :which-key "Harpoon [D]elete")
 
-      "lrr" '(lsp-workspace-restart :which-key "LSP [R]estart")
-      "lrn" '(lsp-rename :which-key "LSP [R]ename")
-      "ls" '(lsp :which-key "LSP [S]tart")
-      "lca" '(lsp-execute-code-action :which-key "LSP [C]ode [A]ction")
-      )
+    "pa" '(pyvenv-activate :which-key "[P]yvenv [A]ctivate")
+    "pd" '(pyvenv-deactivate :which-key "[P]yvenv [D]eactivate")
 
-    (general-define-key
-     :states 'normal
-     "-" #'dired-jump
-     "K" #'lsp-ui-doc-glance
-     "zf" #'evil-toggle-fold)
-
-    (general-define-key
-     :keymaps 'dired-mode-map
-     "-" #'dired-up-directory)
-
-    (general-define-key
-     :keymaps 'vterm-mode-map
-     "C-S-v" #'vterm-yank)
+    "lrr" '(lsp-workspace-restart :which-key "LSP [R]estart")
+    "lrn" '(lsp-rename :which-key "LSP [R]ename")
+    "ls" '(lsp :which-key "LSP [S]tart")
+    "lca" '(lsp-execute-code-action :which-key "LSP [C]ode [A]ction")
     )
 
-  (load custom-file)
+  (general-define-key
+   :states 'normal
+   "-" #'dired-jump
+   "C-c e e" #'eglot
+   "zf" #'evil-toggle-fold)
+
+  (general-define-key
+   :keymaps 'dired-mode-map
+   "-" #'dired-up-directory)
+
+  (general-define-key
+   :keymaps 'vterm-mode-map
+   "C-S-v" #'vterm-yank)
+
+  (general-define-key
+   :keymaps 'eglot-mode-map
+   "C-c e r" #'eglot-rename
+   "C-c e f n" #'flymake-goto-next-error
+   "C-c e f p" #'flymake-goto-prev-error
+   "C-c e c" #'eglot-code-actions
+   "C-c e d" #'xref-find-definitions
+   "C-c e k" #'eldoc
+   )
+  )
+
+(load custom-file)
