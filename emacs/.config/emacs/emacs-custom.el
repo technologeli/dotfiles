79d58bbc8accf6481ec2294fe98f84ac45a5a289
Elijah Lieu <50640682+technologeli@users.noreply.github.com>
2025-05-12 18:19:49 -0400

    uninstall unused packages

--- emacs/.config/emacs/emacs-custom.el
+++ emacs/.config/emacs/emacs-custom.el
@@ -4,11 +4,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(cape company consult-denote corfu denote-journal denote-org
-	  doom-modeline eat ef-themes evil expand-region flycheck
-	  general god-mode lsp-pyright lsp-ui magit marginalia
-	  nerd-icons-dired orderless org-appear org-bullets pyvenv
-	  spacious-padding undo-tree vertico vterm)))
+   '(cape company consult-denote corfu denote-journal denote-org eat
+	  ef-themes expand-region flycheck god-mode magit marginalia
+	  nerd-icons-dired orderless org-appear org-bullets
+	  spacious-padding undo-tree vertico)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
