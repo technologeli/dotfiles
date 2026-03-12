8fdd7d863135635fd356a371473f0c17b811bb48
Elijah Lieu <50640682+technologeli@users.noreply.github.com>
2026-03-11 21:14:09 -0400

    separate note-taking plugin

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,7 +1,5 @@
-local denote_directory = vim.fn.expand("~/notes")
 vim.opt.mouse = ""
 vim.opt.colorcolumn = "80"
-vim.opt.spellfile = vim.fs.joinpath(denote_directory, "20250902T135400--dictionary.en.utf-8.add")
 vim.opt.winborder = "rounded"
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
@@ -75,94 +73,6 @@ local function pick_dotfiles()
 end
 vim.keymap.set("n", "<leader>c", pick_dotfiles)
 
-local external_extensions = {
-	"xlsx", "pptx", "docx",
-	"pdf",
-	"png", "jpg", "jpeg", "gif", "bmp", "svg", "webp"
-}
-
-local function get_notes_list()
-	local items = vim.fn.systemlist("rg --files " .. denote_directory)
-	for i, item in ipairs(items) do
-		items[i] = vim.fn.fnamemodify(item, ":t")
-	end
-	return items
-end
-
-local function pick_notes()
-	MiniPick.start({
-		source = {
-			items = get_notes_list(),
-			name = "Notes",
-			choose = function(filename)
-				local extension = vim.fn.fnamemodify(filename, ":e")
-				if vim.tbl_contains(external_extensions, extension:lower()) then
-					-- open in external app
-					vim.schedule(function()
-						vim.ui.open(vim.fs.joinpath(denote_directory, filename))
-					end)
-				else
-					-- open in neovim
-					vim.schedule(function()
-						vim.cmd("edit " .. vim.fs.joinpath(denote_directory, filename))
-					end)
-				end
-			end,
-		},
-	})
-end
-vim.keymap.set("n", "<leader>nf", pick_notes)
-local function extract_title(filename)
-	local start_pos = filename:find("%-%-") -- Look for "--"
-	if not start_pos then
-		return filename:sub(1, filename:find("T") - 1)
-	end
-
-	-- Adjust to start extracting after "--"
-	start_pos = start_pos + 2
-
-	-- Look for "__" after "--"
-	local end_pos = filename:find("__", start_pos)
-	if not end_pos then
-		-- If "__" not found, look for "."
-		end_pos = filename:find("%.", start_pos)
-	end
-
-	-- If no "__" or ".", return from "--" to end of string
-	if not end_pos then
-		return filename:sub(start_pos)
-	end
-
-	return filename:sub(start_pos, end_pos - 1)
-end
-
-
-local function pick_link()
-	MiniPick.start({
-		source = {
-			items = get_notes_list(),
-			name = "Link to Note",
-			choose = function(filename)
-				vim.schedule(function()
-					vim.ui.input({ prompt = "Link Text: ", default = extract_title(filename) }, function(text)
-						if text == nil then
-							local line = "[" .. extract_title(filename) .. "](./" .. filename .. ")"
-							vim.cmd("normal! i" .. line)
-						else
-							local line = "[" .. text .. "](./" .. filename .. ")"
-							vim.cmd("normal! i" .. line)
-						end
-					end)
-				end)
-			end,
-		},
-	})
-end
-
-vim.keymap.set({ "n", "i" }, "<C-l>", pick_link)
-
-
-
 vim.lsp.config("lua_ls", {
 	settings = {
 		Lua = {
@@ -180,87 +90,6 @@ vim.lsp.enable({ "lua_ls", "clangd", "zls", "basedpyright" })
 vim.keymap.set("n", "<leader>d", vim.lsp.buf.definition)
 vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format)
 vim.cmd("colorscheme gruvbox")
-
-
-local function slugify(str)
-	-- Convert to lowercase, remove punctuation, replace spaces with hyphens
-	str = str:lower():gsub("[^%w%s]", ""):gsub("%s+", "-")
-	return str
-end
-
-local function get_timestamp()
-	return os.date("%Y%m%dT%H%M%S")
-end
-
-local function create_note()
-	vim.ui.input({ prompt = "Title: " }, function(title)
-		local filename = get_timestamp()
-		if title == nil then
-			return
-		end
-
-		if title ~= "" then
-			filename = filename .. "--" .. slugify(title)
-		end
-
-		vim.ui.input({ prompt = "Tags: " }, function(tags)
-			if tags and tags ~= "" then
-				local tag_str = tags:gsub("%s+", ""):gsub(",", "_")
-				filename = filename .. "__" .. tag_str
-			end
-
-			filename = filename .. ".md"
-
-			vim.cmd("edit " .. vim.fs.joinpath(denote_directory, filename))
-		end)
-	end)
-end
-vim.keymap.set("n", "<leader>nn", create_note)
-
-local uv = vim.loop
-
-local function is_created_today(name)
-	local today = os.date("*t")
-
-	return tonumber(string.sub(name, 1, 4)) == today.year
-		and tonumber(string.sub(name, 5, 6)) == today.month
-		and tonumber(string.sub(name, 7, 8)) == today.day
-end
-
-local function today()
-	local files = {}
-	local handle = uv.fs_scandir(denote_directory)
-	if not handle then
-		print("Directory not found: " .. denote_directory)
-		return
-	end
-
-	while true do
-		local name, type = uv.fs_scandir_next(handle)
-		if not name then break end
-		if type == 'file' and name:find("_journal") then
-			local filepath = vim.fs.joinpath(denote_directory, name)
-			local stat = uv.fs_stat(filepath)
-			if is_created_today(name) then
-				table.insert(files, { path = filepath, ctime = stat.ctime.sec })
-			end
-		end
-	end
-
-	if #files == 0 then
-		print("No journal entry for today.")
-		return
-	end
-
-	table.sort(files, function(a, b)
-		return a.ctime > b.ctime
-	end)
-
-	local latest_file = files[1].path
-	vim.cmd("edit " .. vim.fn.fnameescape(latest_file))
-end
-vim.keymap.set("n", "<leader>nt", today)
-
 
 require('render-markdown').setup({
 	heading = { enabled = false },
@@ -284,59 +113,51 @@ vim.api.nvim_create_autocmd("FileType", {
 	end,
 })
 
---vim.keymap.set({ "n", "v", "o" }, "j", function()
---	return vim.v.count > 0 and "j" or ""
---end, { expr = true })
---vim.keymap.set({ "n", "v", "o" }, "k", function()
---	return vim.v.count > 0 and "k" or ""
---end, { expr = true })
-
-vim.api.nvim_create_user_command("DenoteAdopt", function(opts)
-	local original_filename = opts.args
-	if original_filename == "" then
-		vim.notify("No file name given", vim.log.levels.ERROR)
-		return
-	end
-
-	local filename = vim.fn.fnamemodify(original_filename, ":t:r")
-	vim.ui.input({ prompt = "Title (make empty to slugify): ", default = filename }, function(title)
-		if title == nil then
-			return
-		end
-
-		if title == "" then
-			title = slugify(filename)
-		end
-
-		filename = title
-
-		local filetype = vim.fn.fnamemodify(original_filename, ":e")
-		local new_filename = get_timestamp() .. "--" .. filename .. "." .. filetype
-		vim.fn.rename(original_filename, vim.fs.joinpath(denote_directory, new_filename))
-		vim.cmd("redraw")
-		vim.notify("File renamed to " .. new_filename)
-	end)
-end, { nargs = 1, complete = "file" })
-
 vim.keymap.set("n", "<leader>h1", "I# ")
 vim.keymap.set("n", "<leader>h2", "I## ")
 vim.keymap.set("n", "<leader>h3", "I### ")
 vim.keymap.set("n", "<leader>h4", "I#### ")
 vim.keymap.set("n", "<leader>h5", "I##### ")
 vim.keymap.set("n", "<leader>h6", "I###### ")
-vim.keymap.set("n", "<leader>a", "}kA")
-vim.keymap.set("n", "<leader>i", "{jI")
+
+local function paragraph_end_insert()
+  local last_line = vim.fn.line("$")
+  vim.cmd("normal! }")
+  local cur_line = vim.fn.line(".")
+  -- If we jumped to the last line, just append there; otherwise go up one
+  if cur_line < last_line then
+    vim.cmd("normal! k")
+  end
+  vim.cmd("normal! A")
+  vim.cmd("startinsert!")
+end
+
+local function paragraph_start_insert()
+  local first_line = 1
+  vim.cmd("normal! {")
+  local cur_line = vim.fn.line(".")
+  -- If we jumped to the first line, just insert there; otherwise go down one
+  if cur_line > first_line then
+    vim.cmd("normal! j")
+  end
+  vim.cmd("normal! I")
+  vim.cmd("startinsert")
+end
+
+vim.keymap.set("n", "<leader>a", paragraph_end_insert)
+vim.keymap.set("n", "<leader>i", paragraph_start_insert)
 
 require("flash").setup({
 	prompt = { enabled = false }
 })
 
 vim.keymap.set({ "n", "x", "o" }, "<leader>s", function() require("flash").jump() end)
-vim.keymap.set({ "n", "x", "o" }, "<leader>t", function() require("flash").treesitter() end)
+vim.keymap.set({ "n", "x", "o" }, "<leader>v", function() require("flash").treesitter() end) -- v for visual
 vim.keymap.set("o", "r", function() require("flash").remote() end)
 vim.keymap.set({ "o", "x" }, "R", function() require("flash").treesitter_search() end)
 vim.keymap.set("c", "<C-s>", function() require("flash").toggle() end)
 
+-- set makeprg or set mp
 vim.keymap.set("n", "<leader>m", ":make<CR>")
 
 vim.opt.grepprg = "rg --vimgrep"
@@ -347,14 +168,14 @@ vim.api.nvim_create_user_command("Rg", function(opts)
 end, { nargs = "*" })
 
 vim.keymap.set("n", "<leader>e", ":detach<CR>")
-vim.keymap.set("n", "<leader>x", ":terminal<CR>")
+vim.keymap.set("n", "<leader>t", ":terminal<CR>")
 
 vim.keymap.set("n", "<leader>r", function()
 	local old = vim.api.nvim_buf_get_name(0)
-	vim.ui.input({ prompt = "Rename " .. old .. " to: " }, function(new)
+	vim.ui.input({ prompt = "Rename buffer " .. old .. " to: " }, function(new)
 		if new and new ~= "" and new ~= old then
 			vim.fn.rename(old, new)
 			vim.cmd("file " .. vim.fn.fnameescape(new))
 		end
 	end)
-end, { desc = "Rename current file" })
+end)
---
+++ nvim/.config/nvim/plugin/enote.lua
@@ -0,0 +1,279 @@
+-- Like Denote, but Enote, for Eli ;)
+local uv = vim.uv
+local denote_directory = vim.fn.expand("~/notes")
+vim.opt.spellfile = vim.fs.joinpath(denote_directory, "20250902T135400--dictionary.en.utf-8.add")
+
+local external_extensions = {
+	"xlsx", "pptx", "docx",
+	"pdf",
+	"png", "jpg", "jpeg", "gif", "bmp", "svg", "webp"
+}
+
+local function slugify(str)
+	-- Convert to lowercase, remove punctuation, replace spaces with hyphens
+	str = str:lower():gsub("[^%w%s]", ""):gsub("%s+", "-")
+	return str
+end
+
+local function get_timestamp()
+	return os.date("%Y%m%dT%H%M%S")
+end
+
+local function get_notes_list()
+	local items = vim.fn.systemlist("rg --files " .. denote_directory)
+	for i, item in ipairs(items) do
+		items[i] = vim.fn.fnamemodify(item, ":t")
+	end
+	return items
+end
+
+local function prepend_to_file(str, filepath)
+	local lines = vim.fn.readfile(filepath)
+	table.insert(lines, 1, str)
+	vim.fn.writefile(lines, filepath)
+end
+
+local function parse_filename(filename)
+	local part1 = vim.split(filename, "--", { plain = true })
+	local id = part1[1]
+
+	-- tags are optional
+	if filename:find("__") then
+		local part2 = vim.split(part1[2], "__", { plain = true })
+		local title = part2[1]
+		local part3 = vim.split(part2[2], ".", { plain = true })
+		local tags = part3[1]
+		local ext = table.concat(vim.list_slice(part3, 2, #part3), ".")
+		tags = vim.split(tags, "_", { plain = true })
+
+		return {
+			id = id,
+			title = title,
+			tags = tags,
+			ext = ext
+		}
+	else
+		local part2 = vim.split(part1[2], ".", { plain = true })
+		local title = part2[1]
+		local ext = table.concat(vim.list_slice(part2, 2, #part2), ".")
+
+		return {
+			id = id,
+			title = title,
+			tags = nil,
+			ext = ext
+		}
+	end
+end
+
+local function pick_notes()
+	MiniPick.start({
+		source = {
+			items = get_notes_list(),
+			name = "Notes",
+			choose = function(filename)
+				local extension = vim.fn.fnamemodify(filename, ":e")
+				if vim.tbl_contains(external_extensions, extension:lower()) then
+					-- open in external app
+					vim.schedule(function()
+						vim.ui.open(vim.fs.joinpath(denote_directory, filename))
+					end)
+				else
+					-- open in neovim
+					vim.schedule(function()
+						vim.cmd("edit " .. vim.fs.joinpath(denote_directory, filename))
+					end)
+				end
+			end,
+		},
+	})
+end
+
+local function extract_title(filename)
+	local start_pos = filename:find("%-%-") -- Look for "--"
+	if not start_pos then
+		return filename:sub(1, filename:find("T") - 1)
+	end
+
+	-- Adjust to start extracting after "--"
+	start_pos = start_pos + 2
+
+	-- Look for "__" after "--"
+	local end_pos = filename:find("__", start_pos)
+	if not end_pos then
+		-- If "__" not found, look for "."
+		end_pos = filename:find("%.", start_pos)
+	end
+
+	-- If no "__" or ".", return from "--" to end of string
+	if not end_pos then
+		return filename:sub(start_pos)
+	end
+
+	return filename:sub(start_pos, end_pos - 1)
+end
+
+local function pick_link()
+	MiniPick.start({
+		source = {
+			items = get_notes_list(),
+			name = "Link to Note",
+			choose = function(filename)
+				vim.schedule(function()
+					vim.ui.input({ prompt = "Link Text: ", default = extract_title(filename) }, function(text)
+						if text == nil then
+							local line = "[" .. extract_title(filename) .. "](./" .. filename .. ")"
+							vim.cmd("normal! i" .. line)
+						else
+							local line = "[" .. text .. "](./" .. filename .. ")"
+							vim.cmd("normal! i" .. line)
+						end
+					end)
+				end)
+			end,
+		},
+	})
+end
+
+local function create_note()
+	vim.ui.input({ prompt = "Title: " }, function(title)
+		local filename = get_timestamp()
+		if title == nil then
+			return
+		end
+
+		if title ~= "" then
+			filename = filename .. "--" .. slugify(title)
+		end
+
+		vim.ui.input({ prompt = "Tags: " }, function(tags)
+			if tags and tags ~= "" then
+				local tag_str = tags:gsub("%s+", ""):gsub(",", "_")
+				filename = filename .. "__" .. tag_str
+			end
+
+			filename = filename .. ".md"
+
+			vim.cmd("edit " .. vim.fs.joinpath(denote_directory, filename))
+		end)
+	end)
+end
+
+
+local function is_created_today(name)
+	local today = os.date("*t")
+
+	return tonumber(string.sub(name, 1, 4)) == today.year
+		and tonumber(string.sub(name, 5, 6)) == today.month
+		and tonumber(string.sub(name, 7, 8)) == today.day
+end
+
+local function today()
+	local files = {}
+	local handle = uv.fs_scandir(denote_directory)
+	if not handle then
+		print("Directory not found: " .. denote_directory)
+		return
+	end
+
+	while true do
+		local name, type = uv.fs_scandir_next(handle)
+		if not name then break end
+		if type == 'file' and name:find("_journal") then
+			local filepath = vim.fs.joinpath(denote_directory, name)
+			local stat = uv.fs_stat(filepath)
+			if is_created_today(name) then
+				table.insert(files, { path = filepath, ctime = stat.ctime.sec })
+			end
+		end
+	end
+
+	if #files == 0 then
+		print("No journal entry for today.")
+		return
+	end
+
+	table.sort(files, function(a, b)
+		return a.ctime > b.ctime
+	end)
+
+	local latest_file = files[1].path
+	vim.cmd("edit " .. vim.fn.fnameescape(latest_file))
+end
+
+local function scatter(tags, target_directory)
+	tags = vim.split(tags, ",", { plain = true })
+	vim.notify("Files copied:")
+	for name, type in vim.fs.dir(denote_directory) do
+		if type == "file" then
+			local file = parse_filename(name)
+			if file.tags ~= nil then
+				local matches_all_tags = true
+				for _, tag in ipairs(tags) do
+					if not vim.tbl_contains(file.tags, tag) then
+						matches_all_tags = false
+					end
+				end
+
+				if matches_all_tags and vim.tbl_contains(file.tags, "shareable") then
+					local new_name = file.title .. "." .. file.ext
+					local new_path = vim.fs.joinpath(target_directory, new_name)
+					local old_path = vim.fs.joinpath(denote_directory, name)
+
+					local success = uv.fs_copyfile(old_path, new_path)
+					if success then
+						vim.notify("- " .. new_name)
+					end
+
+					-- don't put a date here because git will think it's a changed file
+					if file.ext == "md" then
+						prepend_to_file("<!-- This file was copied (autogenerated) from my notes. -->", new_path)
+					end
+				end
+			end
+		end
+	end
+end
+
+vim.api.nvim_create_user_command("Scatter", function(opts)
+	if #opts.fargs ~= 2 then
+		vim.notify("Usage: :Scatter <tag> <target_directory>")
+		return
+	end
+	local tag = opts.fargs[1]
+	local target_directory = vim.fn.expand(opts.fargs[2])
+	scatter(tag, target_directory)
+end, { nargs = "+", complete = "dir" })
+
+vim.api.nvim_create_user_command("DenoteAdopt", function(opts)
+	local original_filename = opts.args
+	if original_filename == "" then
+		vim.notify("No file name given", vim.log.levels.ERROR)
+		return
+	end
+
+	local filename = vim.fn.fnamemodify(original_filename, ":t:r")
+	vim.ui.input({ prompt = "Title (make empty to slugify): ", default = filename }, function(title)
+		if title == nil then
+			return
+		end
+
+		if title == "" then
+			title = slugify(filename)
+		end
+
+		filename = title
+
+		local filetype = vim.fn.fnamemodify(original_filename, ":e")
+		local new_filename = get_timestamp() .. "--" .. filename .. "." .. filetype
+		vim.fn.rename(original_filename, vim.fs.joinpath(denote_directory, new_filename))
+		vim.cmd("redraw")
+		vim.notify("File renamed to " .. new_filename)
+	end)
+end, { nargs = 1, complete = "file" })
+
+
+vim.keymap.set({ "n", "i" }, "<C-l>", pick_link)
+vim.keymap.set("n", "<leader>nf", pick_notes)
+vim.keymap.set("n", "<leader>nn", create_note)
+vim.keymap.set("n", "<leader>nt", today)
